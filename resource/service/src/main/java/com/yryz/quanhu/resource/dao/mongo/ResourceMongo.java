94b76b3072b1293157603b89176ba8f6bab45bf8
yehao <yehao@yryz.com>
2018-01-17 14:56:18 +0800

    清除无用的引用

--- resource/service/src/main/java/com/yryz/quanhu/resource/dao/mongo/ResourceMongo.java
+++ resource/service/src/main/java/com/yryz/quanhu/resource/dao/mongo/ResourceMongo.java
@@ -14,7 +14,6 @@ import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.domain.Sort.Order;
--- resource/service/src/test/java/com/yryz/quanhu/resource/ResourceTest.java
+++ resource/service/src/test/java/com/yryz/quanhu/resource/ResourceTest.java
@@ -12,10 +12,8 @@ import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.yryz.common.utils.GsonUtils;
