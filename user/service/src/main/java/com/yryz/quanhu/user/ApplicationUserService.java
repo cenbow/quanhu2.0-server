5e1468614df20f320927d751e0824c5e72a1a8b4
xiepeng <pxiebray@gmail.com>
2018-01-17 12:10:10 +0800

    elasticjob 集成demo

--- user/service/src/main/java/com/yryz/quanhu/user/ApplicationUserService.java
+++ user/service/src/main/java/com/yryz/quanhu/user/ApplicationUserService.java
@@ -2,11 +2,12 @@ package com.yryz.quanhu.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@ImportResource("classpath:META-INF/spring.xml")
 public class ApplicationUserService {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationUserService.class, args);
--- user/service/src/main/java/com/yryz/quanhu/user/job/DemoJob.java
+++ user/service/src/main/java/com/yryz/quanhu/user/job/DemoJob.java
@@ -1,9 +1,29 @@
 package com.yryz.quanhu.user.job;
-
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 /**
- * @author Administrator
+ * @author xiepeng
  * @version 1.0
- * @data 2018/1/17 0017 58
+ * @data 2018/1/17 0017 08
  */
-public class DemoJob {
+public class DemoJob implements SimpleJob{
+    private static final Log logger = LogFactory.getLog(DemoJob.class);
+    @Override
+    public void execute(ShardingContext shardingContext) {
+        logger.info("elastic job demo..." + shardingContext.getJobName());
+
+        switch (shardingContext.getShardingItem()) {
+            case 0:
+                // do something by sharding item 0
+                break;
+            case 1:
+                // do something by sharding item 1
+                break;
+            default:
+                // do something by sharding default
+                break;
+        }
+    }
 }
