955d3b7a57a5a5ed6388f7d9980383d8252a0024
xiepeng <pxiebray@gmail.com>
2018-01-17 18:38:53 +0800

    添加elasticJob使用引导说明

--- demo/service/src/main/java/com/yryz/quanhu/demo/job/DemoJob.java
+++ demo/service/src/main/java/com/yryz/quanhu/demo/job/DemoJob.java
@@ -9,6 +9,29 @@ import org.apache.commons.logging.LogFactory;
  * @version 1.0
  * @data 2018/1/17 0017 08
  */
+
+/**
+使用引导
+1，确认项目POM依赖中引入以下配置
+    <dependency>
+        <groupId>com.dangdang</groupId>
+        <artifactId>elastic-job-lite-core</artifactId>
+    </dependency>
+    <dependency>
+        <groupId>com.dangdang</groupId>
+        <artifactId>elastic-job-lite-spring</artifactId>
+    </dependency>
+
+2，使用spring xml方式配置注册中心及定时任务
+   确认boot启动类引入了xml配置  @ImportResource("classpath:META-INF/spring.xml")
+   elasticJob的springxml配置使用方式，详细见spring.xml示例或参考官方文档
+
+3，application.properties配置
+   job.reg.address=127.0.0.1:2181   zookeeper注册地址
+   job.reg.namespace=demo-job       项目级空间命名，同一个项目或服务使用同一管理空间
+*/
+
+
 public class DemoJob implements SimpleJob{
     private static final Log logger = LogFactory.getLog(DemoJob.class);
     @Override
