a6b502fbb586850330ecf534a2efa709f61d1f73
danshiyu <danshiyu@yryz.com>
2018-01-20 17:23:07 +0800

    1.调试commonSafe

---
+++ message/service/src/test/java/com/yryz/quanhu/message/CommonSafeTest.java
@@ -0,0 +1,38 @@
+package com.yryz.quanhu.message;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.yryz.common.utils.JsonUtils;
+import com.yryz.quanhu.message.commonsafe.api.CommonSafeApi;
+import com.yryz.quanhu.message.commonsafe.constants.CommonServiceType;
+import com.yryz.quanhu.message.commonsafe.dto.VerifyCodeDTO;
+import com.yryz.quanhu.message.commonsafe.vo.VerifyCodeVO;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommonSafeTest {
+	 @Autowired
+	 CommonSafeApi safeApi;
+	 
+	 //@Test
+	 public void sendVerifyCode(){
+		 VerifyCodeDTO codeDTO = new VerifyCodeDTO(1, CommonServiceType.PHONE_VERIFYCODE_SEND.getName(), "15088052677", "vebff12m1762");
+		 
+		 VerifyCodeVO codeVO = safeApi.getVerifyCode(codeDTO).getData();
+		 System.out.println(JsonUtils.toFastJson(codeVO));
+	 }
+	 
+	 @Test
+	 public void checkVerifyCode(){
+		 VerifyCodeDTO codeDTO = new VerifyCodeDTO(1, CommonServiceType.PHONE_VERIFYCODE_SEND.getName(), "15088052677", "vebff12m1762","9831");
+		 
+		 int result = safeApi.checkVerifyCode(codeDTO).getData();
+		
+		 
+		 System.out.println("reslut:"+result);
+	 }
+}
