d4e00c2d8a5c3adc6b72ad3fdc4d327173d9b911
王恒 <wangheng@yryz.com>
2018-01-23 22:45:14 +0800

    ﻿[模块名称]：平台文章发布、模板获取 api 基版 [JIRA单号]： [描述]：

---
+++ resource/api/src/main/java/com/yryz/quanhu/resource/release/info/vo/ReleaseInfoVo.java
@@ -0,0 +1,14 @@
+package com.yryz.quanhu.resource.release.info.vo;
+
+import com.yryz.quanhu.resource.release.info.entity.ReleaseInfo;
+
+/**
+* @Description: 文章
+* @author wangheng
+* @date 2018年1月22日 下午4:46:32
+*/
+public class ReleaseInfoVo extends ReleaseInfo {
+
+    private static final long serialVersionUID = 1L;
+
+}
