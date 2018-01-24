23ea8a6327881adfaf8fb6abab3370fe5bca7114
lifan <Lifan@yryz.com>
2018-01-24 14:06:28 +0800

    完成订单SDK的功能

---
+++ order/order-sdk/src/main/java/com/yryz/quanhu/order/sdk/constant/OrderType.java
@@ -0,0 +1,44 @@
+package com.yryz.quanhu.order.sdk.constant;
+
+/**
+ * 订单相关常量类
+ *
+ * @author admin
+ */
+public class OrderType {
+
+    /**
+     * 加钱
+     */
+    public static Integer ORDER_TYPE_PLUS = 1;
+
+    /**
+     * 减钱
+     */
+    public static Integer ORDER_TYPE_MINUS = 0;
+
+    /**
+     * 1，余额；
+     */
+    public static Integer TYPE_ONE = 1;
+
+    /**
+     * 2，积分；
+     */
+    public static Integer TYPE_TWO = 2;
+
+    /**
+     * 3，混合
+     */
+    public static Integer TYPE_THREE = 3;
+
+    /**
+     * 创建
+     */
+    public static Integer ORDER_STATE_CREATE = 0;
+
+    /**
+     * 完成
+     */
+    public static Integer ORDER_STATE_SUCCESS = 1;
+}
