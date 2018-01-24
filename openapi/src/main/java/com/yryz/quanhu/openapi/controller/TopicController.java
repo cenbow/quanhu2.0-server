288b92111b471b788664638cabfbff384ca733f6
wanghongtao <abel0307@126.com>
2018-01-24 22:17:04 +0800

    增加：01、话题主页(话题详情)    01、帖子的详情    02、发布帖子
       01、圈粉发布问题，付费提问时，圈粉支付提问费用
       02、圈粉删除问题

---
+++ openapi/src/main/java/com/yryz/quanhu/openapi/controller/TopicController.java
@@ -0,0 +1,40 @@
+package com.yryz.quanhu.openapi.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.yryz.common.entity.RequestHeader;
+import com.yryz.common.response.Response;
+import com.yryz.common.utils.WebUtil;
+import com.yryz.quanhu.openapi.ApplicationOpenApi;
+import com.yryz.quanhu.resource.questionsAnswers.api.QuestionApi;
+import com.yryz.quanhu.resource.questionsAnswers.dto.QuestionDto;
+import com.yryz.quanhu.resource.questionsAnswers.vo.QuestionVo;
+import com.yryz.quanhu.resource.topic.api.TopicApi;
+import com.yryz.quanhu.resource.topic.entity.Topic;
+import com.yryz.quanhu.resource.topic.vo.TopicVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Api(description = "话题接口")
+@RestController
+public class TopicController {
+
+	@Reference
+	private TopicApi topicApi;
+
+
+	@ApiOperation("查询话题主页")
+	@ApiImplicitParam(name = "version", paramType = "path", allowableValues = ApplicationOpenApi.CURRENT_VERSION, required = true)
+	@GetMapping(value = "/{version}/topic/single")
+	public Response<TopicVo> queryTopicDetail(Long kid, HttpServletRequest request) {
+		RequestHeader header = WebUtil.getHeader(request);
+		return topicApi.queryDetail(kid,Long.valueOf(header.getUserId()));
+	}
+
+}
