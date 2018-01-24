bdd7c9d24317c9314cc69b2f541f3dc19bd89929
sunyao <sunyao@yryz.com>
2018-01-24 18:08:25 +0800

    增加点赞API

---
+++ openapi/src/main/java/com/yryz/quanhu/openapi/controller/LikeController.java
@@ -0,0 +1,51 @@
+package com.yryz.quanhu.openapi.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.yryz.common.annotation.NotLogin;
+import com.yryz.common.response.Response;
+import com.yryz.quanhu.behavior.comment.entity.Comment;
+import com.yryz.quanhu.behavior.like.Service.LikeApi;
+import com.yryz.quanhu.behavior.like.entity.Like;
+import com.yryz.quanhu.behavior.like.vo.LikeVO;
+import com.yryz.quanhu.openapi.ApplicationOpenApi;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author:sun
+ * @version:2.0
+ * @Description:点赞
+ * @Date:Created in 16:57 2018/1/24
+ */
+@Api(tags = "点赞")
+@RestController
+public class LikeController {
+
+    @Reference(check = false)
+    private LikeApi likeApi;
+
+    @NotLogin
+    @ApiOperation("用户点赞/取消点赞")
+    @ApiImplicitParam(name = "version", paramType = "path", allowableValues = ApplicationOpenApi.CURRENT_VERSION, required = true)
+    @PostMapping(value = "/services/app/{version}/like/dian")
+    public Response<Map<String, Object>> accretion(@RequestBody Like like, @RequestHeader Long userId) {
+        like.setUserId(userId);
+        return likeApi.dian(like);
+    }
+
+    @NotLogin
+    @ApiOperation("点赞列表")
+    @ApiImplicitParam(name = "version", paramType = "path", allowableValues = ApplicationOpenApi.CURRENT_VERSION, required = true)
+    @GetMapping(value = "/services/app/{version}/like/frontlist")
+    public Response<List<LikeVO>> queryLikers(Integer resourceId){
+        Like like=new Like();
+        like.setResourceId(resourceId);
+        return likeApi.queryLikers(like);
+    }
+
+}
