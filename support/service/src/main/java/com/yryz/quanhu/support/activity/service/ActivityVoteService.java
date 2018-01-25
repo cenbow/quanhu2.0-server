123fe39b353917813e49d6cdd799b4424a1f9124
jiangzhichao <jiangzhichao@yryz.com>
2018-01-25 21:01:45 +0800

    提交活动相关方法

--- support/service/src/main/java/com/yryz/quanhu/support/activity/service/ActivityVoteService.java
+++ support/service/src/main/java/com/yryz/quanhu/support/activity/service/ActivityVoteService.java
@@ -1,7 +1,10 @@
 package com.yryz.quanhu.support.activity.service;
 
+import com.yryz.common.response.PageList;
+import com.yryz.quanhu.support.activity.dto.ActivityVoteDto;
 import com.yryz.quanhu.support.activity.entity.ActivityVoteConfig;
 import com.yryz.quanhu.support.activity.entity.ActivityVoteRecord;
+import com.yryz.quanhu.support.activity.vo.ActivityPrizesVo;
 import com.yryz.quanhu.support.activity.vo.ActivityVoteInfoVo;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -43,5 +46,12 @@ public interface ActivityVoteService {
      * @return
      * */
     int selectUserRoll(Long createUserId);
+
+    /**
+     *  奖品列表
+     *  @param  activityVoteDto
+     *  @return
+     * */
+    PageList<ActivityPrizesVo> prizesList(ActivityVoteDto activityVoteDto);
 
 }
