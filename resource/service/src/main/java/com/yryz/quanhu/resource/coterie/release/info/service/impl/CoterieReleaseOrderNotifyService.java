48c9b92944d574834b99c40be1f36e80875ab03d
王恒 <wangheng@yryz.com>
2018-01-25 10:17:15 +0800

    ﻿[模块名称]：resource 模块解除 order 依赖 [JIRA单号]： [描述]：

--- resource/service/src/main/java/com/yryz/quanhu/resource/coterie/release/info/service/impl/CoterieReleaseOrderNotifyService.java
+++ resource/service/src/main/java/com/yryz/quanhu/resource/coterie/release/info/service/impl/CoterieReleaseOrderNotifyService.java
@@ -2,8 +2,6 @@ package com.yryz.quanhu.resource.coterie.release.info.service.impl;
 
 import org.springframework.stereotype.Service;
 
-import com.yryz.quanhu.order.sdk.IOrderNotifyService;
-import com.yryz.quanhu.order.sdk.dto.OutputOrder;
 
 /**
 * @Description: 付费阅读文章，订单回调
@@ -11,9 +9,9 @@ import com.yryz.quanhu.order.sdk.dto.OutputOrder;
 * @date 2018年1月24日 下午9:06:17
 */
 @Service
-public class CoterieReleaseOrderNotifyService implements IOrderNotifyService {
+public class CoterieReleaseOrderNotifyService /*implements IOrderNotifyService*/ {
 
-    @Override
-    public void notify(OutputOrder outputOrder) {
-    }
+//    @Override
+//    public void notify(OutputOrder outputOrder) {
+//    }
 }
