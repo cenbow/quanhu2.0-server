838ef51d3e875386056ee241f8164aea259d6d57
wanghongtao <abel0307@126.com>
2018-01-25 10:23:31 +0800

    发帖拿掉用户的userId 检验

--- openapi/src/main/java/com/yryz/quanhu/openapi/controller/PostController.java
+++ openapi/src/main/java/com/yryz/quanhu/openapi/controller/PostController.java
@@ -13,6 +13,7 @@ import com.yryz.quanhu.resource.topic.vo.TopicPostVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,42 +22,44 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class PostController {
 
-	@Reference
-	private TopicPostApi topicPostApi;
+    @Reference
+    private TopicPostApi topicPostApi;
 
 
-	@ApiOperation("查询帖子详情")
-	@ApiImplicitParam(name = "version", paramType = "path", allowableValues = ApplicationOpenApi.CURRENT_VERSION, required = true)
-	@GetMapping(value = "/{version}/post/single")
-	public Response<TopicAndPostVo> queryTopicDetail(Long kid, HttpServletRequest request) {
-		RequestHeader header = WebUtil.getHeader(request);
-		return topicPostApi.quetyDetail(kid,null);
-	}
+    @ApiOperation("查询帖子详情")
+    @ApiImplicitParam(name = "version", paramType = "path", allowableValues = ApplicationOpenApi.CURRENT_VERSION, required = true)
+    @GetMapping(value = "/{version}/post/single")
+    public Response<TopicAndPostVo> queryTopicDetail(Long kid, HttpServletRequest request) {
+        RequestHeader header = WebUtil.getHeader(request);
+        return topicPostApi.quetyDetail(kid, null);
+    }
 
-	@ApiOperation("查询帖子列表")
-	@ApiImplicitParam(name = "version", paramType = "path", allowableValues = ApplicationOpenApi.CURRENT_VERSION, required = true)
-	@GetMapping(value = "/{version}/post/list")
-	public Response<PageList<TopicPostVo>> queryTopicPostList(Integer pageNum,Integer 	pageSize,Long topicId,
-														  String orderBy,HttpServletRequest request) {
-		RequestHeader header = WebUtil.getHeader(request);
-		TopicPostDto dto=new TopicPostDto();
-		dto.setPageNum(pageNum);
-		dto.setPageSize(pageSize);
-		dto.setTopicId(topicId);
-		dto.setOrderBy(orderBy);
-		return topicPostApi.listPost(dto);
-	}
+    @ApiOperation("查询帖子列表")
+    @ApiImplicitParam(name = "version", paramType = "path", allowableValues = ApplicationOpenApi.CURRENT_VERSION, required = true)
+    @GetMapping(value = "/{version}/post/list")
+    public Response<PageList<TopicPostVo>> queryTopicPostList(Integer pageNum, Integer pageSize, Long topicId,
+                                                              String orderBy, HttpServletRequest request) {
+        RequestHeader header = WebUtil.getHeader(request);
+        TopicPostDto dto = new TopicPostDto();
+        dto.setPageNum(pageNum);
+        dto.setPageSize(pageSize);
+        dto.setTopicId(topicId);
+        dto.setOrderBy(orderBy);
+        return topicPostApi.listPost(dto);
+    }
 
 
-	@ApiOperation("发布帖子")
-	@ApiImplicitParam(name = "version", paramType = "path", allowableValues =ApplicationOpenApi.CURRENT_VERSION, required = true)
-	@PostMapping(value = "/{version}/post/add")
-	public Response<TopicPostVo> saveTopic(@RequestBody  TopicPostDto topicPostDto, HttpServletRequest request) {
-		RequestHeader header = WebUtil.getHeader(request);
-		topicPostDto.setCreateUserId(Long.valueOf(header.getUserId()));
-		return topicPostApi.savePost(topicPostDto);
-	}
-
+    @ApiOperation("发布帖子")
+    @ApiImplicitParam(name = "version", paramType = "path", allowableValues = ApplicationOpenApi.CURRENT_VERSION, required = true)
+    @PostMapping(value = "/{version}/post/add")
+    public Response<TopicPostVo> saveTopic(@RequestBody TopicPostDto topicPostDto, HttpServletRequest request) {
+        RequestHeader header = WebUtil.getHeader(request);
+        String userId = header.getUserId();
+        if (StringUtils.isNotBlank(userId)) {
+            topicPostDto.setCreateUserId(Long.valueOf(header.getUserId()));
+        }
+        return topicPostApi.savePost(topicPostDto);
+    }
 
 
 }
