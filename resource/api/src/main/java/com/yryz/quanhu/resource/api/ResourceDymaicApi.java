4b136d936da603aced5c5bbe800e996aec1cacc3
liupan <xfliupan@163.com>
2018-01-27 16:44:57 +0800

    资源聚合API注释

--- resource/api/src/main/java/com/yryz/quanhu/resource/api/ResourceDymaicApi.java
+++ resource/api/src/main/java/com/yryz/quanhu/resource/api/ResourceDymaicApi.java
@@ -11,6 +11,11 @@ import com.yryz.quanhu.resource.vo.ResourceTotal;
  */
 public interface ResourceDymaicApi {
 
+    /**
+     * 提交聚合资源和动态
+     * @param resourceTotal 请尽量把对象内的字段都填充上，若有不清楚的请咨询刘攀
+     * @return
+     */
     public Response<Object> commitResourceDymaic(ResourceTotal resourceTotal);
 
 }
