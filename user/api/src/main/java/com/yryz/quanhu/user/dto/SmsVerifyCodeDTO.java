0eaa789f1426c5d71937affff8d5eec854a0cb2f
zhangkun <zhangkun@yryz.com>
2018-01-29 13:25:49 +0800

    阿里滑动验证码

--- user/api/src/main/java/com/yryz/quanhu/user/dto/SmsVerifyCodeDTO.java
+++ user/api/src/main/java/com/yryz/quanhu/user/dto/SmsVerifyCodeDTO.java
@@ -24,6 +24,47 @@ public class SmsVerifyCodeDTO implements Serializable{
 	 * 用户id
 	 */
 	private Long userId;
+
+	/**
+	 * 阿里验证码请求参数
+	 */
+	private String session;
+	private String sig;
+	private String token;
+	private String scene;
+
+	public String getSession() {
+		return session;
+	}
+
+	public void setSession(String session) {
+		this.session = session;
+	}
+
+	public String getSig() {
+		return sig;
+	}
+
+	public void setSig(String sig) {
+		this.sig = sig;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getScene() {
+		return scene;
+	}
+
+	public void setScene(String scene) {
+		this.scene = scene;
+	}
+
 	public String getPhone() {
 		return phone;
 	}
