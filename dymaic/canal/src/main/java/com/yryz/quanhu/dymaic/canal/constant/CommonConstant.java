375bfc3fd4c3db6aba00f4dd62d4c0dddb123be2
zhangkun <zhangkun@yryz.com>
2018-01-29 13:27:57 +0800

    达人列表接入最新动态

--- dymaic/canal/src/main/java/com/yryz/quanhu/dymaic/canal/constant/CommonConstant.java
+++ dymaic/canal/src/main/java/com/yryz/quanhu/dymaic/canal/constant/CommonConstant.java
@@ -1,30 +1,33 @@
 package com.yryz.quanhu.dymaic.canal.constant;
 
 /**
- * 
  * @author jk
  */
 public class CommonConstant {
-    public final static int BATCH_SIZE = 2*1024;
-    
-    public class QuanHuDb{
-    	public final static String DB_NAME="db1";
-        public final static String TABLE_USER="qh_user_baseinfo";
-        public final static String TABLE_TOPIC="qh_topic";
-        public final static String TABLE_TOPIC_POST="qh_topic_post";
-        public final static String TABLE_RELEASE_INFO="qh_release_info";
-        public final static String TABLE_COTERIE="qh_coterie";
+    public final static int BATCH_SIZE = 2 * 1024;
+
+    public class QuanHuDb {
+        public final static String DB_NAME = "db1";
+        public final static String TABLE_USER = "qh_user_baseinfo";
+        public final static String TABLE_TOPIC = "qh_topic";
+        public final static String TABLE_TOPIC_POST = "qh_topic_post";
+        public final static String TABLE_RELEASE_INFO = "qh_release_info";
+        public final static String TABLE_COTERIE = "qh_coterie";
+        //用户标签
+        public final static String TABLE_USER_TAG = "qh_user_tag";
+
+
         /**
          * MongoDB 热度数据变更直接发送的mq消息
          */
-        public final static String TABLE_MONGODB_RESOURCE_HEAT="mongodb_resource_heat";
+        public final static String TABLE_MONGODB_RESOURCE_HEAT = "mongodb_resource_heat";
     }
-    
-    
-    public class EventType{
-    	public final static String OPT_UPDATE="update";
-        public final static String OPT_DELETE="delete";
-        public final static String OPT_INSERT="insert";
+
+
+    public class EventType {
+        public final static String OPT_UPDATE = "update";
+        public final static String OPT_DELETE = "delete";
+        public final static String OPT_INSERT = "insert";
     }
-    
+
 }
