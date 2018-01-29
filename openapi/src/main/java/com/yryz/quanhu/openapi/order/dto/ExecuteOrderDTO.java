ea7f44b437f9e26de8bfd38b8a63c04174edb6c6
lifan <Lifan@yryz.com>
2018-01-29 17:19:50 +0800

    修改执行订单接口参数

--- openapi/src/main/java/com/yryz/quanhu/openapi/order/dto/ExecuteOrderDTO.java
+++ openapi/src/main/java/com/yryz/quanhu/openapi/order/dto/ExecuteOrderDTO.java
@@ -1,15 +1,15 @@
 /**
  * Copyright (c) 2018-2019 Wuhan Yryz Network Company LTD.
  * All rights reserved.
- * 
+ * <p>
  * Created on 2018年1月23日
  * Id: ExecuteOrderDTO.java, 2018年1月23日 上午11:34:26 yehao
  */
 package com.yryz.quanhu.openapi.order.dto;
 
-import java.io.Serializable;
-
 import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
 
 /**
  * @author yehao
@@ -18,65 +18,66 @@ import io.swagger.annotations.ApiModelProperty;
  * @Description 支付订单的DTO
  */
 public class ExecuteOrderDTO implements Serializable {
-	
-	/**
-	 * 订单ID
-	 */
-	@ApiModelProperty("订单ID")
-	private String orderId;
-	
-	/**
-	 * 用户ID
-	 */
-	@ApiModelProperty("用户ID")
-	private String custId; 
-	
-	/**
-	 * 支付密码
-	 */
-	@ApiModelProperty("支付密码")
-	private String payPassord;
 
-	/**
-	 * @return the orderId
-	 */
-	public String getOrderId() {
-		return orderId;
-	}
+    private static final long serialVersionUID = 8226876268508831154L;
+    /**
+     * 订单ID
+     */
+    @ApiModelProperty("订单ID")
+    private String orderId;
 
-	/**
-	 * @param orderId the orderId to set
-	 */
-	public void setOrderId(String orderId) {
-		this.orderId = orderId;
-	}
+    /**
+     * 用户ID
+     */
+    @ApiModelProperty(value = "用户ID", hidden = true)
+    private String custId;
 
-	/**
-	 * @return the custId
-	 */
-	public String getCustId() {
-		return custId;
-	}
+    /**
+     * 支付密码
+     */
+    @ApiModelProperty("支付密码")
+    private String payPassord;
 
-	/**
-	 * @param custId the custId to set
-	 */
-	public void setCustId(String custId) {
-		this.custId = custId;
-	}
+    /**
+     * @return the orderId
+     */
+    public String getOrderId() {
+        return orderId;
+    }
 
-	/**
-	 * @return the payPassord
-	 */
-	public String getPayPassord() {
-		return payPassord;
-	}
+    /**
+     * @param orderId the orderId to set
+     */
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
 
-	/**
-	 * @param payPassord the payPassord to set
-	 */
-	public void setPayPassord(String payPassord) {
-		this.payPassord = payPassord;
-	}
-	
+    /**
+     * @return the custId
+     */
+    public String getCustId() {
+        return custId;
+    }
+
+    /**
+     * @param custId the custId to set
+     */
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    /**
+     * @return the payPassord
+     */
+    public String getPayPassord() {
+        return payPassord;
+    }
+
+    /**
+     * @param payPassord the payPassord to set
+     */
+    public void setPayPassord(String payPassord) {
+        this.payPassord = payPassord;
+    }
+
 }
