e2ba52217143f46388c3c4eb7bad4c414fc79127
jiangkun <jiangkun@yryz.com>
2018-02-01 16:24:25 +0800

    canal 服务单独部署

--- dymaic/elasticsearch/src/main/java/com/yryz/quanhu/dymaic/canal/dao/CoterieInfoRepository.java
+++ dymaic/elasticsearch/src/main/java/com/yryz/quanhu/dymaic/canal/dao/CoterieInfoRepository.java
@@ -1,9 +1,11 @@
 package com.yryz.quanhu.dymaic.canal.dao;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import com.yryz.quanhu.dymaic.canal.entity.CoterieInfo;
 
+@Repository
 public interface CoterieInfoRepository extends ElasticsearchRepository<CoterieInfo, Long>,CoterieInfoSearch {
 
 }
