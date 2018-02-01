3f2184ca6e86610fc92ede675d73f6af9c871622
lifan <Lifan@yryz.com>
2018-02-01 11:16:44 +0800

    完成打赏消息的对接

--- openapi/src/main/java/com/yryz/quanhu/openapi/controller/RewardController.java
+++ openapi/src/main/java/com/yryz/quanhu/openapi/controller/RewardController.java
@@ -1,16 +1,5 @@
 package com.yryz.quanhu.openapi.controller;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.yryz.common.annotation.UserBehaviorValidation;
 import com.yryz.common.constant.ExceptionEnum;
@@ -27,17 +16,20 @@ import com.yryz.quanhu.behavior.reward.entity.RewardInfo;
 import com.yryz.quanhu.behavior.reward.vo.RewardFlowVo;
 import com.yryz.quanhu.behavior.reward.vo.RewardInfoVo;
 import com.yryz.quanhu.openapi.ApplicationOpenApi;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
-* @Description: 打赏
-* @author wangheng
-* @date 2018年1月29日 下午3:34:56
-*/
+ * @author wangheng
+ * @Description: 打赏
+ * @date 2018年1月29日 下午3:34:56
+ */
 @Api(description = "打赏接口")
 @RestController
 @RequestMapping(value = "/services/app")
@@ -54,11 +46,11 @@ public class RewardController {
             @ApiImplicitParam(name = "version", paramType = "path", allowableValues = ApplicationOpenApi.CURRENT_VERSION, required = true),
             @ApiImplicitParam(name = "record", paramType = "body", required = true),
             @ApiImplicitParam(name = "userId", paramType = "header", required = true),
-            @ApiImplicitParam(name = "token", paramType = "header", required = true) })
+            @ApiImplicitParam(name = "token", paramType = "header", required = true)})
     @UserBehaviorValidation(event = "资源打赏", login = true)
     @PostMapping(value = "{version}/reward")
     public Response<Map<String, Object>> reward(HttpServletRequest request, @RequestBody RewardInfo record,
-            @RequestHeader("userId") Long headerUserId) {
+                                                @RequestHeader("userId") Long headerUserId) {
 
         record.setCreateUserId(headerUserId);
 
@@ -69,11 +61,11 @@ public class RewardController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "version", paramType = "path", allowableValues = ApplicationOpenApi.CURRENT_VERSION, required = true),
             @ApiImplicitParam(name = "userId", paramType = "header", required = true),
-            @ApiImplicitParam(name = "token", paramType = "header", required = true) })
+            @ApiImplicitParam(name = "token", paramType = "header", required = true)})
     @UserBehaviorValidation(event = "打赏列表", login = true)
     @GetMapping(value = "{version}/reward/list")
     public Response<PageList<RewardInfoVo>> list(HttpServletRequest request, RewardInfoDto dto,
-            @RequestHeader("userId") Long headerUserId) {
+                                                 @RequestHeader("userId") Long headerUserId) {
 
         if (RewardConstants.QueryType.my_reward_user_list.equals(dto.getQueryType())
                 || RewardConstants.QueryType.my_reward_resource_list.equals(dto.getQueryType())) {
@@ -90,11 +82,19 @@ public class RewardController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "version", paramType = "path", allowableValues = ApplicationOpenApi.CURRENT_VERSION, required = true),
             @ApiImplicitParam(name = "userId", paramType = "header", required = true),
-            @ApiImplicitParam(name = "token", paramType = "header", required = true) })
+            @ApiImplicitParam(name = "token", paramType = "header", required = true)})
     @UserBehaviorValidation(event = "我打赏/收到打赏 金额", login = true)
     @GetMapping(value = "{version}/reward/amount")
     public Response<RewardCount> amountCount(HttpServletRequest request, @RequestHeader("userId") Long headerUserId) {
         RewardCount rewardCount = ResponseUtils.getResponseData(rewardCountApi.selectByTargetId(headerUserId));
+        if (null == rewardCount) {
+            rewardCount = new RewardCount();
+            rewardCount.setTargetId(headerUserId);
+            rewardCount.setTotalRewardAmount(0L);
+            rewardCount.setTotalRewardCount(0);
+            rewardCount.setTotalRewardedAmount(0L);
+            rewardCount.setTotalRewardedCount(0);
+        }
         return ResponseUtils.returnObjectSuccess(rewardCount);
     }
 
@@ -102,11 +102,11 @@ public class RewardController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "version", paramType = "path", allowableValues = ApplicationOpenApi.CURRENT_VERSION, required = true),
             @ApiImplicitParam(name = "userId", paramType = "header", required = true),
-            @ApiImplicitParam(name = "token", paramType = "header", required = true) })
+            @ApiImplicitParam(name = "token", paramType = "header", required = true)})
     @UserBehaviorValidation(event = "打赏明细流水", login = true)
     @GetMapping(value = "{version}/reward/flow")
     public Response<PageList<RewardFlowVo>> listFlow(HttpServletRequest request,
-            @RequestHeader("userId") Long headerUserId, Integer currentPage, Integer pageSize) {
+                                                     @RequestHeader("userId") Long headerUserId, Integer currentPage, Integer pageSize) {
         if (null == headerUserId) {
             throw new QuanhuException(ExceptionEnum.ValidateException.getCode(),
                     ExceptionEnum.ValidateException.getShowMsg(), "用户ID不能为空");
