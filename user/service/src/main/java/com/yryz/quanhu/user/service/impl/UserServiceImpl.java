f6c8d06058f615e7bb0fb14c3e7956e831707911
danshiyu <danshiyu@yryz.com>
2018-02-01 15:15:26 +0800

    1.修复用户数据查询

--- user/service/src/main/java/com/yryz/quanhu/user/service/impl/UserServiceImpl.java
+++ user/service/src/main/java/com/yryz/quanhu/user/service/impl/UserServiceImpl.java
@@ -168,7 +168,7 @@ public class UserServiceImpl implements UserService {
 			simpleVO.setRelationStatus(relationDto.getRelationStatus());
 		}
 		// 依赖积分系统，获取用户等级
-		EventAcount acount = eventManager.getGrow(userId.toString());
+		EventAcount acount = eventManager.getGrow(friendId.toString());
 		if (acount == null || NumberUtils.toLong(acount.getGrowLevel()) < 1) {
 			simpleVO.setUserLevel("1");
 		} else {
