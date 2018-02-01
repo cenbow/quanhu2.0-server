61fda8c7bc064bf36b10c8a36f6eaf69c5899bf4
pengnian <pengnian@yryz.com>
2018-02-01 14:50:48 +0800

    消息模块

---
+++ message/service/src/main/java/com/yryz/quanhu/message/message/mq/MessageSender.java
@@ -0,0 +1,40 @@
+package com.yryz.quanhu.message.message.mq;
+
+import com.yryz.quanhu.message.message.constants.MessageMQConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Copyright (c) 2017-2018 Wuhan Yryz Network Company LTD.
+ * All rights reserved.
+ *
+ * @Description:
+ * @Date: Created in 2018 2018/1/31 10:22
+ * @Author: pn
+ */
+@Service
+public class MessageSender {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MessageSender.class);
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * 指定单一队列
+     *
+     * @param msg
+     */
+    public void send(String msg) {
+        LOGGER.info("开始发送管理后台消息到MQ：" + msg);
+        rabbitTemplate.setExchange(MessageMQConstant.MESSAGE_EXCHANGE);
+        rabbitTemplate.setRoutingKey(MessageMQConstant.MESSAGE_QUEUE);
+        rabbitTemplate.convertAndSend(msg);
+        LOGGER.info("完成发送管理后台消息到MQ：" + msg);
+    }
+
+
+}
---
+++ message/service/src/test/java/com/yryz/quanhu/message/MessageAdminTest.java
@@ -0,0 +1,60 @@
+package com.yryz.quanhu.message;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.yryz.common.message.*;
+import com.yryz.common.response.Response;
+import com.yryz.common.response.ResponseUtils;
+import com.yryz.common.utils.DateUtils;
+import com.yryz.quanhu.message.message.api.MessageAdminAPI;
+import com.yryz.quanhu.message.message.constants.MessageContants;
+import com.yryz.quanhu.message.message.vo.MessageAdminVo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Copyright (c) 2017-2018 Wuhan Yryz Network Company LTD.
+ * All rights reserved.
+ *
+ * @Description:
+ * @Date: Created in 2018 2018/2/1 11:53
+ * @Author: pn
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MessageAdminTest {
+
+    @Reference(check = false, timeout = 30000, retries = 0)
+    private MessageAdminAPI messageAdminAPI;
+
+    @Test
+    public void push() {
+        MessageAdminVo messageAdminVo = new MessageAdminVo();
+        messageAdminVo.setMessageSource(MessageContants.MESSAGE_SOURCE_WRITE);
+        messageAdminVo.setTitle("啊哈哈哈哈哈");
+        messageAdminVo.setContent("测试没钱没钱没钱没钱吗mqmq");
+        messageAdminVo.setImg("http://www.hao123.com");
+        messageAdminVo.setLink("http://www.hao123.com");
+        messageAdminVo.setPersistentType(MessageContants.PERSISTENT);
+        messageAdminVo.setPushType(MessageContants.PUSH_TYPE_START);
+        messageAdminVo.setType(MessageType.RECOMMEND_TYPE);
+        messageAdminVo.setLabel(MessageLabel.RECOMMEND_CONTENT);
+        messageAdminVo.setViewCode(MessageViewCode.RECOMMEND_MESSAGE_1);
+        messageAdminVo.setViewCode(MessageActionCode.INNER_URL);
+        messageAdminVo.setCreateTime(DateUtils.getDateTime());
+        //messageAdminVo.setPushDate("2018-02-01 14:15:00");
+        messageAdminVo.setPushDate(DateUtils.getDateTime());
+
+        List<String> list = new ArrayList<>();
+        list.add("726907134491074560");
+        messageAdminVo.setPushUserIds(list);
+        Response<Boolean> booleanResponse = messageAdminAPI.push(messageAdminVo);
+        System.out.println(ResponseUtils.getResponseNotNull(booleanResponse));
+    }
+
+}
