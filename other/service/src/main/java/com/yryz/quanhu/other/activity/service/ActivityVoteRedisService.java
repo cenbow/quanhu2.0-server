901d0cbbb037cb24d29b0812de31e88f49f450c4
jiangzhichao <jiangzhichao@yryz.com>
2018-02-05 10:52:25 +0800

    调整活动管理后台接口

--- other/service/src/main/java/com/yryz/quanhu/other/activity/service/ActivityVoteRedisService.java
+++ other/service/src/main/java/com/yryz/quanhu/other/activity/service/ActivityVoteRedisService.java
@@ -1,5 +1,7 @@
 package com.yryz.quanhu.other.activity.service;
 
+import org.springframework.data.redis.core.RedisTemplate;
+
 public interface ActivityVoteRedisService {
 
     /**
@@ -10,6 +12,16 @@ public interface ActivityVoteRedisService {
      * @param   totalCount           总票数
      * */
     void addCandidate(Long activityInfoId, Long detailKid, Long detailId, Long totalCount);
+
+    /**
+     * 上架参与者
+     * @param   activityInfoId      活动id
+     * @param   detailKid            参与者kid
+     * @param   detailId             参与者id
+     * @param   totalCount           总票数
+     * @param   template
+     * */
+    void shelvesCandidate(Long activityInfoId, Long detailKid, Long detailId, Long totalCount, RedisTemplate<String, Long> template);
 
     /**
      * 删除参与者
