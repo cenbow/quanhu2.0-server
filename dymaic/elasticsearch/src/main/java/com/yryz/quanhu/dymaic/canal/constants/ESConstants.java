aa2a42a2bf5fca45f38af3781bdca34fa63bb89e
huangxy <hxy_coding@163.com>
2018-02-06 18:38:53 +0800

    1，添加User后台ES查询条件

--- dymaic/elasticsearch/src/main/java/com/yryz/quanhu/dymaic/canal/constants/ESConstants.java
+++ dymaic/elasticsearch/src/main/java/com/yryz/quanhu/dymaic/canal/constants/ESConstants.java
@@ -13,11 +13,14 @@ public class ESConstants {
     public static final String USER_PHONE = "userBaseInfo.userPhone";
     public static final String USER_STATUS = "userBaseInfo.userStatus";
     public static final String BAN_POST_TIME = "userBaseInfo.banPostTime";
-    
+    public static final String USER_ROLE = "userBaseInfo.userRole";
+
     //达人认证信息
     public static final String STAR_AUDITSTATUS = "userStarInfo.auditStatus";
     public static final String STAR_AUTHTYPE = "userStarInfo.authType";
     public static final String STAR_AUTHWAY = "userStarInfo.authWay";
+    public static final String STAR_RECOMMENDSTATUS = "userStarInfo.recommendStatus";
+
     //等级
     public static final String EVENT_GROWLEVEL = "eventAccountInfo.growLevel";
 
