a26a396716799b77cc6a27d976349c7401d7a27a
yehao <yehao@yryz.com>
2018-02-08 12:01:29 +0800

    修复userId的Long改为String

--- resource/api/src/main/java/com/yryz/quanhu/resource/vo/ResourceVo.java
+++ resource/api/src/main/java/com/yryz/quanhu/resource/vo/ResourceVo.java
@@ -11,6 +11,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Map;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.yryz.quanhu.coterie.coterie.vo.Coterie;
 import com.yryz.quanhu.coterie.coterie.vo.CoterieBasicInfo;
 import com.yryz.quanhu.coterie.coterie.vo.CoterieInfo;
@@ -36,11 +38,13 @@ public class ResourceVo implements Serializable {
     /**
      * 动态ID
      */
+	@JsonSerialize(using = ToStringSerializer.class)
     private Long kid;
 
     /**
      * 用户ID
      */
+	@JsonSerialize(using = ToStringSerializer.class)
     private Long userId;
 
     /**
